18d29f2fdb955ac3631618c89d2e488e1455ac6a
Md-Atiqur-Rahman <atiqur.himel@selise.ch>
2025-02-14 21:06:42 +0600

    Securing Ocelot API Gateway with Bearer Token

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Values;
+using System.Reflection.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Ocelot.json configuration file
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+
+builder.Services.AddAuthentication().AddJwtBearer("IdentityApiKey", x =>
+{
+    x.Authority = "https://localhost:5005";
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false
+    };
+});
+
 
 // Add Ocelot services
 builder.Services.AddOcelot(builder.Configuration);
