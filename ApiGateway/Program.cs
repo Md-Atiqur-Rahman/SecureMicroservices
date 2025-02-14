af69cac0475df9f4480759b83565b473530b4f4c
Md-Atiqur-Rahman <atiqur.himel@selise.ch>
2025-02-14 20:59:10 +0600

    Building Ocelot API Gateway Microservice

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,6 +1,25 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Ocelot.json configuration file
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add Ocelot services
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+// Use Ocelot middleware
+await app.UseOcelot();
 
 app.Run();
