2f59946eb9e74c6220cf87f755a85d9b1a1df35b
Md-Atiqur-Rahman <atiqur.himel@selise.ch>
2025-02-14 13:20:25 +0600

    Authorize Movies Page

--- Movies.Client/Controllers/MoviesController.cs
+++ Movies.Client/Controllers/MoviesController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Movies.Client.ApiServices;
 using Movies.Client.Models;
 
 namespace Movies.Client.Controllers
 {
+    [Authorize]
     public class MoviesController : Controller
     {
         private readonly IMovieAPIService _movieAPIService;
