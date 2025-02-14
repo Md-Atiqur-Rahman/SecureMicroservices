9961424901cb5170ddc71a6a68e9ca3a4927591b
Md-Atiqur-Rahman <atiqur.himel@selise.ch>
2025-02-14 10:45:28 +0600

    Building MVC Client Application

---
+++ Movies.Client/ApiServices/IMovieAPIService.cs
@@ -0,0 +1,16 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices;
+
+public interface IMovieAPIService
+{
+    Task<IEnumerable<Movie>> GetMovies();
+
+    Task<Movie> GetMovie(int id);
+
+    Task<bool> CreateMovie(Movie movie);
+
+    Task<bool> UpdateMovie(int id, Movie movie);
+
+    Task<bool> DeleteMovie(int id);
+}
---
+++ Movies.Client/ApiServices/MovieAPIService.cs
@@ -0,0 +1,42 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices;
+
+public class MovieAPIService : IMovieAPIService
+{
+    public Task<bool> CreateMovie(Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteMovie(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Movie> GetMovie(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Movie>> GetMovies()
+    {
+        var movieList = new List<Movie>();
+        movieList.Add(new Movie
+        {
+            Id=1,
+            Genre="Comic",
+            Title="asd",
+            Rating="9.2",
+            ImageUrl="image/resource",
+            ReleaseDate = DateTime.Now,
+            Owner="swn"
+        });
+        return await Task.FromResult(movieList);
+    }
+
+    public Task<bool> UpdateMovie(int id, Movie movie)
+    {
+        throw new NotImplementedException();
+    }
+}
